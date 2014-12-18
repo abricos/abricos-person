69d9867e4eb0f30fda850c747497d620943bfddd
Alexander Kuzmin <roosit@abricos.org>
2014-12-18 18:18:32 +0300

    add person template

--- src/includes/config.pub.php
+++ src/includes/config.pub.php
@@ -1,9 +1,30 @@
 <?php
 
 $config['phrase']['sys'] = array(
-    "phone" => "",
-    "email" => "",
-    "address" => ""
+    "site_name" => "Abricos Person"
+);
+
+$config['Template'] = array(
+    // по умолчанию использовать шаблон blog из стиля default
+    "default" => array(
+        "owner" => "person",
+        "name" => "main"
+    ),
+    "ignore" => array(
+        array(
+            "pattern" => "/^\/bos\//i",
+            "regexp" => true
+        )
+    ),
+    "exp" => array(
+        // использовать шаблон main из стиля default для главной страницы сайта
+        array(
+            "pattern" => "/",
+            "regexp" => false,
+            "owner" => "person",
+            "name" => "home"
+        )
+    )
 );
 
 ?>
